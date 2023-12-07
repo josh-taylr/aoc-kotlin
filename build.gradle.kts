848b8784b16ccf2380fc26b7c5d4c24d47406ccf
Joshua Taylor <4213664+josh-taylr@users.noreply.github.com>
2023-12-07 23:56:16 +0000

    Add ktfmt Gradle Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    id("com.ncorti.ktfmt.gradle") version "0.15.1"
 }
 
 sourceSets {
     main {
         kotlin.srcDir("src")
     }
+}
+
+ktfmt {
+    kotlinLangStyle()
 }
 
 tasks {
