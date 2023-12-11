466e81272a44321da05df7e411ae1217d2331505
Joshua Taylor <4213664+josh-taylr@users.noreply.github.com>
2023-12-11 14:51:18 +0000

    Refactor Day 11: Cosmic Expansion

--- src/Day11.kt
+++ src/Day11.kt
@@ -3,12 +3,13 @@ import kotlin.math.min
 
 fun main() {
     fun part1(input: List<String>): Long {
-        val universe = Universe.parse(2, input)
+        val universe = Universe.parse(input)
         return universe.sumOfShortestPaths()
     }
 
     fun part2(input: List<String>): Long {
-        val universe = Universe.parse(1_000_000, input)
+        val universe = Universe.parse(input)
+        universe.expansionValue = 1_000_000
         return universe.sumOfShortestPaths()
     }
 
@@ -20,9 +21,10 @@ fun main() {
 }
 
 private class Universe(
-        val galaxies: List<Point>,
-        val rowMultiplier: List<Int>,
-        val colMultiplier: List<Int>,
+    private val galaxies: List<Point>,
+    private val emptyRows: MutableList<Boolean>,
+    private val emptyColumns: MutableList<Boolean>,
+    var expansionValue: Int = 2,
 ) {
 
     fun sumOfShortestPaths(): Long {
@@ -30,7 +32,7 @@ private class Universe(
         var sum = 0L
         for (i in galaxies.indices) {
             for (j in i + 1 until galaxies.size) {
-                val shortest = galaxies[i].manhattanDistance(galaxies[j], rowMultiplier, colMultiplier)
+                val shortest = galaxies[i].manhattanDistance(galaxies[j])
                 if (sum <= Long.MAX_VALUE - shortest) {
                     sum += shortest
                 } else {
@@ -41,32 +43,30 @@ private class Universe(
         return sum
     }
 
+    private fun Point.manhattanDistance(other: Point) =
+        (emptyColumns.map(::toExpansion).subList(min(x, other.x), max(x, other.x)).sum()) +
+            (emptyRows.map(::toExpansion).subList(min(y, other.y), max(y, other.y)).sum())
+
+    private fun toExpansion(isEmpty: Boolean) = if (isEmpty) expansionValue else 1
+
     companion object {
-        fun parse(expansionValue: Int, input: List<String>): Universe {
-            val rowMultiplier = MutableList(input.count()) { expansionValue }
-            val colMultiplier = MutableList(input.first().count()) { expansionValue }
+        fun parse(input: List<String>): Universe {
+            val emptyRows = MutableList(input.count()) { true }
+            val emptyColumns = MutableList(input.first().count()) { true }
             val galaxies = buildList {
                 for (row in input.indices) {
                     for (col in input[row].indices) {
                         when (input[row][col]) {
                             '#' -> {
-                                colMultiplier[col] = 1
-                                rowMultiplier[row] = 1
+                                emptyColumns[col] = false
+                                emptyRows[row] = false
                                 add(Point(col, row))
                             }
                         }
                     }
                 }
             }
-            return Universe(galaxies, rowMultiplier, colMultiplier)
+            return Universe(galaxies, emptyRows, emptyColumns)
         }
     }
 }
-
-private fun Point.manhattanDistance(
-    other: Point,
-    rowMultiplier: List<Int>,
-    colMultiplier: List<Int>
-) =
-    (colMultiplier.subList(min(x, other.x), max(x, other.x)).sum()) +
-        (rowMultiplier.subList(min(y, other.y), max(y, other.y)).sum())
