2c3e716530209a12552d987031395716ff100495
Joshua Taylor <4213664+josh-taylr@users.noreply.github.com>
2023-12-13 00:12:44 +0000

    WIP solution for day 12, part 2

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,68 +1,34 @@
-import kotlin.math.pow
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 
 fun main() {
     fun part1(input: List<String>): Int {
         return input.map(String::toRecords).sumOf { (record, criteria) ->
-            var count = 0
-            val unknown = record.getUnknown()
-            // generate all permutations of record where unknown are set to either '#' or '.'
-            for (i in 0 until 2.0.pow(unknown.size).toInt()) {
-                val permutation =
-                    unknown
-                        .foldIndexed(record.toCharArray()) { idx, perm, pos ->
-                            perm.apply { set(pos, if (i and (1 shl idx) != 0) '#' else '.') }
-                        }
-                        .concatToString()
-                if (permutation.isFixed(criteria)) count++
-            }
-            count
+            val permutations = findStart(record.toCharArray(), criteria.toIntArray())
+            permutations.count()
         }
     }
 
     fun part2(input: List<String>): Int = runBlocking {
-        input
-            .map { it.toRecords().expand() }
-            .mapIndexed { recordId, (record, criteria) ->
-                async(CoroutineName("record-${record.take(10)}") + Dispatchers.IO) {
-                    val unknown = record.getUnknown()
-                    val total = (2.0.pow(unknown.size).toInt())
-                    val tenth = total / 10
-                    var nextPrint = tenth
-                    var count = 0
-                    var i = 0
-                    // generate all permutations of record where unknown are set to either '#' or
-                    // '.'
-                    println("Record $recordId, Permutation: 0 / $total")
-                    while (isActive && i < total) {
-                        if (i > nextPrint) {
-                            println("Record $recordId, Permutation: $i / $total")
-                            nextPrint += tenth
+        input.map { it.toRecords().expand(times = 4) }
+                .chunked(64)
+                .map { chunked ->
+                    async(Dispatchers.IO) {
+                        chunked.sumOf { (record, criteria) ->
+                            val permutations = findStart(record.toCharArray(), criteria.toIntArray())
+                            permutations.count()
                         }
-                        val permutation =
-                            unknown
-                                .foldIndexed(record.toCharArray()) { idx, perm, pos ->
-                                    perm.apply {
-                                        set(pos, if (i and (1 shl idx) != 0) '#' else '.')
-                                    }
-                                }
-                                .concatToString()
-                        if (permutation.isFixed(criteria)) count++
-
-                        i += 1
                     }
-                    count
-                }
-            }
-            .awaitAll()
-            .sum()
+                }.awaitAll().sum()
     }
 
     checkValue(21, part1(readInput("Day12_test")))
 
     val input = readInput("Day12")
     part1(input).println()
-    //    part2(input).println()
+    part2(input).println()
 }
 
 private fun String.toRecords(): Pair<String, List<Int>> {
@@ -72,24 +38,75 @@ private fun String.toRecords(): Pair<String, List<Int>> {
     return damagedConditions to groupSizes
 }
 
-private fun Pair<String, List<Int>>.expand(): Pair<String, List<Int>> {
-    val repetitions = 5
-    val expandedRecord = first.repeat(repetitions)
-    val expandedCriteria = List(second.count() * repetitions) { second[it % second.count()] }
+private fun Pair<String, List<Int>>.expand(times: Int): Pair<String, List<Int>> {
+    val expandedRecord = List(times) { first }.joinToString("?")
+    val expandedCriteria = List(second.count() * times) { second[it % second.count()] }
     return expandedRecord to expandedCriteria
 }
 
-private fun String.getUnknown() =
-    foldIndexed(emptyList<Int>()) { idx, unknown, c -> if (c == '?') unknown + idx else unknown }
-
-private fun String.isFixed(criteria: List<Int>): Boolean {
-    // build a regular expression for the criteria
-    // e.g. criteria: 1,1,3 -> regex: /^[.]*[#]{1}[.]+[#]{1}[.]+[#]{3}[.]*$/
-    val regex =
-        criteria
-            .joinToString(separator = "[.]+", prefix = "^[.]*", postfix = "[.]*\$") { groupSize ->
-                "[#]{$groupSize}"
+private fun findStart(record: CharArray, criteria: IntArray): List<CharArray> {
+    val remaining = criteria.sum() + (criteria.lastIndex)
+    return if (record.size < remaining)
+        emptyList()
+    else if (criteria.isEmpty() && record.contains('#'))
+        emptyList()
+    else if (record.isEmpty() && criteria.isNotEmpty())
+        emptyList()
+    else if (record.isEmpty() || criteria.isEmpty())
+        listOf(charArrayOf())
+    else when (val c = record.first()) {
+        '#' -> if (criteria.first() > 1)
+            consumeGroup(record.copyOfRange(1, record.size), criteria.copyOf().apply { this[0]-- }).map { charArrayOf('#') + it }
+        else
+            endGroup(record.copyOfRange(1, record.size), criteria.copyOfRange(1, criteria.size)).map { charArrayOf('#') + it }
+        '.' -> findStart(record.copyOfRange(1, record.size), criteria).map { charArrayOf('.') }
+        '?' -> if (criteria.first() > 1)
+            buildList {
+                addAll(consumeGroup(record.copyOfRange(1, record.size), criteria.copyOf().apply { this[0]-- }).map { charArrayOf('#') + it })
+                addAll(findStart(record.copyOfRange(1, record.size), criteria).map { charArrayOf('.') })
             }
-            .toRegex()
-    return matches(regex)
+        else
+            buildList {
+                addAll(endGroup(record.copyOfRange(1, record.size), criteria.copyOfRange(1, criteria.size)).map { charArrayOf('#') + it })
+                addAll(findStart(record.copyOfRange(1, record.size), criteria).map { charArrayOf('.') + it })
+            }
+        else -> error("Received unexpected character: $c")
+    }
+}
+
+private fun consumeGroup(record: CharArray, criteria: IntArray): List<CharArray> {
+    val remaining = criteria.sum() + (criteria.lastIndex)
+    return if (record.size < remaining)
+        emptyList()
+    else if (criteria.isEmpty() && record.contains('#'))
+        emptyList()
+    else if (record.isEmpty() && criteria.isNotEmpty())
+        emptyList()
+    else if (record.isEmpty() || criteria.isEmpty())
+        listOf(charArrayOf())
+    else when (val c = record.first()) {
+        '#', '?' -> if (criteria.first() > 1)
+            consumeGroup(record.copyOfRange(1, record.size), criteria.copyOf().apply { this[0]-- }).map { charArrayOf('#') + it }
+        else
+            endGroup(record.copyOfRange(1, record.size), criteria.copyOfRange(1, criteria.size)).map { charArrayOf('#') + it }
+        '.' -> emptyList()
+        else -> error("Received unexpected character: $c")
+    }
+}
+
+private fun endGroup(record: CharArray, criteria: IntArray): List<CharArray> {
+    val remaining = criteria.sum() + (criteria.lastIndex)
+    return if (record.size < remaining)
+        emptyList()
+    else if (criteria.isEmpty() && record.contains('#'))
+        emptyList()
+    else if (record.isEmpty() && criteria.isNotEmpty())
+        emptyList()
+    else if (record.isEmpty() || criteria.isEmpty())
+        listOf(charArrayOf())
+    else when (val c = record.first()) {
+        '.', '?' -> findStart(record.copyOfRange(1, record.size), criteria).map { charArrayOf('.') }
+        '#' -> emptyList()
+        else -> error("Received unexpected character: $c")
+    }
 }
