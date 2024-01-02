1cd885da473edb7610f3c929c03cec327a8e09f6
Joshua Taylor <4213664+josh-taylr@users.noreply.github.com>
2024-01-02 02:30:12 +0000

    Add solution to day 9, part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,25 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.map(String::toIntList).sumOf { calcNextNumber(it) }
-    }
+    fun part1(input: List<String>) = input.map(String::toIntList).sumOf { it.extrapolate().last() }
 
-    fun part2(input: List<String>): Int {
-        return Int.MAX_VALUE
-    }
+    fun part2(input: List<String>) = input.map(String::toIntList).sumOf { it.extrapolate().first() }
 
     checkValue(114, part1(readInput("Day09_test")))
 
     val input = readInput("Day09")
     part1(input).println()
+    part2(input).println()
 }
 
-private fun calcNextNumber(numbers: List<Int>): Int {
-    val accumulatedDeltas =
-        buildList<List<Int>> {
-            add(numbers)
-            while (!last().all { it == 0 }) { // while not all 0s
-                add(last().deltas())
-            }
+private fun List<Int>.extrapolate(): List<Int> =
+    accumulateDeltas().reduceRight { numbers, deltas ->
+        buildList {
+            add(numbers.first() - deltas.first())
+            addAll(numbers)
+            add(numbers.last() + deltas.last())
         }
-    return accumulatedDeltas.foldRight(0) { deltas, result -> deltas.last() + result }
-}
+    }
+
+private fun List<Int>.accumulateDeltas() =
+    generateSequence(this) { it.deltas().takeUnless { deltas -> deltas.all { n -> n == 0 } } }
+        .toList()
