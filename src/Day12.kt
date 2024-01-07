1b7b25af5b0313444bc2c355c0aab73f20158fa9
Joshua Taylor <4213664+josh-taylr@users.noreply.github.com>
2024-01-08 00:16:41 +0100

    Refactor the parsers state machine

--- src/Day12.kt
+++ src/Day12.kt
@@ -4,34 +4,31 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 
-private const val debug = true
-
 fun main() {
     fun part1(input: List<String>): Int =
-        input.map(String::toRecords).sumOf { (damaged, criteria) ->
-            println()
-            damaged.countPossibleArrangements(criteria)
-        }
+            input.map(String::toRecords).sumOf { (damaged, criteria) ->
+                damaged.countPossibleArrangements(criteria)
+            }
 
     fun part2(input: List<String>): Int = runBlocking {
         input.map { it.toRecords().expand(times = 4) }
-            .chunked(32)
-            .map { chunked ->
-                async(Dispatchers.IO) {
-                    chunked.sumOf { (damaged, criteria) ->
-                        damaged.countPossibleArrangements(criteria)
+                .chunked(32)
+                .map { chunked ->
+                    async(Dispatchers.IO) {
+                        chunked.sumOf { (damaged, criteria) ->
+                            damaged.countPossibleArrangements(criteria)
+                        }
                     }
                 }
-            }
-            .awaitAll()
-            .sum()
+                .awaitAll()
+                .sum()
     }
 
     checkValue(21, part1(readInput("Day12_test")))
 
     val input = readInput("Day12")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
 
 private fun String.toRecords(): Pair<String, List<Int>> {
@@ -47,93 +44,53 @@ private fun Pair<String, List<Int>>.expand(times: Int): Pair<String, List<Int>> 
     return expandedRecord to expandedCriteria
 }
 
-private fun String.countPossibleArrangements(criteria: List<Int>): Int =
-    findStart(
-        record = toCharArray(),
-        recordStart = 0,
-        criteria = criteria.toIntArray(),
-        criteriaStart = 0
-    )
-
 private const val ImpossibleRecord = 0
 private const val FixedRecord = 1
 
-private fun findStart(
-    record: CharArray,
-    recordStart: Int,
-    criteria: IntArray,
-    criteriaStart: Int
-): Int = when {
-    isNotPossible(record, recordStart, criteria, criteriaStart) -> ImpossibleRecord
-    else -> {
-        val c = record[recordStart]
-        sum(
-                if (c == '#' || c == '?') criteria.runWithDecrement(criteriaStart) { decrementedCriteria ->
-                    record[recordStart] = '#'
-                    val res = if (decrementedCriteria[criteriaStart] > 0)
-                        consumeGroup(record, recordStart.inc(), decrementedCriteria, criteriaStart)
-                    else
-                        endGroup(record, recordStart.inc(), decrementedCriteria, criteriaStart.inc())
-                    record[recordStart] = c
-                    res
-                } else 0,
-
-                if (c == '.' || c == '?') {
-                    record[recordStart] = '.'
-                    val res = findStart(record, recordStart.inc(), criteria, criteriaStart)
-                    record[recordStart] = c
-                    res
-                } else 0
+private fun String.countPossibleArrangements(criteria: List<Int>): Int =
+        findStart(
+                record = toCharArray(),
+                recordStart = 0,
+                criteria = criteria.toIntArray(),
+                criteriaStart = 0
         )
-    }
-}
+
+private fun findStart(
+        record: CharArray,
+        recordStart: Int,
+        criteria: IntArray,
+        criteriaStart: Int
+)= sum(
+        when (record.getOrNull(recordStart)) {
+            '#', '?' -> assumeBroken(criteria, criteriaStart, record, recordStart)
+            else -> 0
+        },
+        when (record.getOrNull(recordStart)) {
+            '.', '?' -> assumeOperational(record, recordStart, criteria, criteriaStart)
+            else -> 0
+        }
+)
 
 private fun consumeGroup(
-    record: CharArray,
-    recordStart: Int,
-    criteria: IntArray,
-    criteriaStart: Int
-): Int = when {
-    isNotPossible(record, recordStart, criteria, criteriaStart) -> ImpossibleRecord
-    else -> {
-        val c = record[recordStart]
-        if (c == '#' || c == '?') criteria.runWithDecrement(criteriaStart) { decrementedCriteria ->
-            record[recordStart] = '#'
-            val res = if (decrementedCriteria[criteriaStart] > 0)
-                consumeGroup(record, recordStart.inc(), decrementedCriteria, criteriaStart)
-            else
-                endGroup(record, recordStart.inc(), decrementedCriteria, criteriaStart.inc())
-            record[recordStart] = c
-            res
-        } else
-            ImpossibleRecord
-    }
+        record: CharArray,
+        recordStart: Int,
+        criteria: IntArray,
+        criteriaStart: Int
+): Int = when (record.getOrNull(recordStart)) {
+    '#', '?' -> assumeBroken(criteria, criteriaStart, record, recordStart)
+    null -> endGroup(record, recordStart, criteria, criteriaStart)
+    else -> ImpossibleRecord
 }
 
 private fun endGroup(
-    record: CharArray,
-    recordStart: Int,
-    criteria: IntArray,
-    criteriaStart: Int
-): Int = when {
-    isNotPossible(record, recordStart, criteria, criteriaStart) -> ImpossibleRecord
-    isFixed(record, recordStart, criteria, criteriaStart) -> {
-        if (debug) record.concatToString().println()
-        FixedRecord
-    }
-    else -> {
-        val c = record[recordStart]
-        if (c == '.' || c == '?') {
-            record[recordStart] = '.'
-            val res = if (criteria.last() > 0)
-                findStart(record, recordStart.inc(), criteria, criteriaStart)
-            else
-                findEnd(record, recordStart.inc(), criteria, criteriaStart)
-            record[recordStart] = c
-            res
-        } else
-            ImpossibleRecord
-    }
+        record: CharArray,
+        recordStart: Int,
+        criteria: IntArray,
+        criteriaStart: Int
+): Int = when (record.getOrNull(recordStart)) {
+    '.', '?' -> assumeOperational(record, recordStart, criteria, criteriaStart)
+    null -> if (criteria.last() > 0) ImpossibleRecord else FixedRecord
+    else -> ImpossibleRecord
 }
 
 private fun findEnd(
@@ -141,44 +98,36 @@ private fun findEnd(
         recordStart: Int,
         criteria: IntArray,
         criteriaStart: Int
-): Int = when {
-    isFixed(record, recordStart, criteria, criteriaStart) -> {
-        if (debug) record.concatToString().println()
-        FixedRecord
+): Int = when (record.getOrNull(recordStart)) {
+    '.', '?' -> assumeOperational(record, recordStart, criteria, criteriaStart)
+    null -> FixedRecord
+    else -> ImpossibleRecord
+}
+
+private fun assumeOperational(record: CharArray, recordStart: Int, criteria: IntArray, criteriaStart: Int): Int {
+    var res: Int
+    record[recordStart] = record[recordStart].also {
+        record[recordStart] = '.'
+        res = if (criteria.last() > 0)
+            findStart(record, recordStart.inc(), criteria, criteriaStart)
+        else
+            findEnd(record, recordStart.inc(), criteria, criteriaStart)
     }
-    else -> {
-        val c = record[recordStart]
-        if (c == '.' || c == '?') {
-            record[recordStart] = '.'
-            val res = findEnd(record, recordStart.inc(), criteria, criteriaStart)
-            record[recordStart] = c
+    return res
+}
+
+private fun assumeBroken(criteria: IntArray, criteriaStart: Int, record: CharArray, recordStart: Int): Int =
+        criteria.runWithDecrement(criteriaStart) { decrementedCriteria ->
+            var res: Int
+            record[recordStart] = record[recordStart].also {
+                record[recordStart] = '#'
+                res = if (decrementedCriteria[criteriaStart] > 0)
+                    consumeGroup(record, recordStart.inc(), decrementedCriteria, criteriaStart)
+                else
+                    endGroup(record, recordStart.inc(), decrementedCriteria, criteriaStart.inc())
+            }
             res
-        } else
-            ImpossibleRecord
-    }
-}
-
-private fun isNotPossible(
-    record: CharArray,
-    recordStart: Int,
-    criteria: IntArray,
-    criteriaStart: Int
-): Boolean {
-    val hasConsumedRecord = recordStart > record.lastIndex
-    val hasConsumedCriteria = criteriaStart > criteria.lastIndex
-    return hasConsumedRecord && !hasConsumedCriteria
-}
-
-private fun isFixed(
-    record: CharArray,
-    recordStart: Int,
-    criteria: IntArray,
-    criteriaStart: Int
-): Boolean {
-    val hasConsumedRecord = recordStart > record.lastIndex
-    val hasConsumedCriteria = criteriaStart > criteria.lastIndex
-    return hasConsumedRecord && hasConsumedCriteria
-}
+        }
 
 private inline fun <R> IntArray.runWithDecrement(index: Int = 0, action: (IntArray) -> R): R {
     this[index] -= 1
